af11d71fcbd73488fb0f83cb6d2a56d6320fc8cc
lizzymcruz <leizylmcruz@gmail.com>
2025-10-07 06:12:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const movieSearch = document.getElementById('movie-search')
 movieSearch.addEventListener('submit', function(e){
     e.preventDefault()
 
-    fetch('')
+    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=8ea9847d')
 })
