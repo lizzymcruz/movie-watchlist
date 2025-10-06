75d225111c9c3d392fd5ffdaf5505400128def78
lizzymcruz <leizylmcruz@gmail.com>
2025-10-07 06:09:55 +0800

    update id

---
+++ index.js
@@ -0,0 +1,8 @@
+const movieInput = document.getElementById('movie-search-input')
+const movieSearch = document.getElementById('movie-search')
+
+movieSearch.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    fetch('')
+})
